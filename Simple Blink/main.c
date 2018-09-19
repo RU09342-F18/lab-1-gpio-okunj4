7944f3dd1b24c690187ba38f62cbf7f167d04ecc
okunj4 <36168902+okunj4@users.noreply.github.com>
2018-09-19 15:52:45 -0400

    Add files via upload

---
+++ Simple Blink/main.c
@@ -0,0 +1,26 @@
+#include <msp430.h>
+int main()
+{
+    WDTCTL = WDTPW + WDTHOLD; //Stop Watchdog timer
+
+    P1SEL &= ~BIT0;
+    P1SEL2 &= ~BIT0;
+    P1DIR |= BIT0;
+
+
+
+
+    for (;;)
+      {
+        volatile unsigned int i;
+
+        P1OUT ^= BIT0;                          // Toggle P1.0 using exclusive-OR
+
+        i = 50000;                              // Delay
+        do (i--);
+        while (i != 0);
+      }
+
+
+    return(0);
+}
