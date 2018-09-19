d90da7ba1a056b7cd6c7274cd4b559a9682421d1
okunj4 <36168902+okunj4@users.noreply.github.com>
2018-09-19 15:58:36 -0400

    Add files via upload

---
+++ Button Blink/main.c
@@ -0,0 +1,18 @@
+#include <msp430.h>
+void main(void)
+{
+    WDTCTL = WDTPW + WDTHOLD; //stop watchdog timer
+
+    P1DIR = BIT0; //Sets Port 1 to output pin
+    P1REN = BIT3; //Enables pull-up resistor for Port 1
+    P1OUT = BIT3; //Sets Port 1 output to normally high by connecting to pull up resistor
+
+    while(1)
+    {
+        if((P1IN & BIT3)!=BIT3)
+        {
+            _delay_cycles(250000); //sets delay so LED can be seen with button press
+            P1OUT ^= BIT0;
+        }
+    }
+}
