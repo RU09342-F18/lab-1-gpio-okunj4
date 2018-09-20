54b706d630bccda7ce17a54121e32353a71cc0dd
okunj4 <36168902+okunj4@users.noreply.github.com>
2018-09-20 15:42:32 -0400

    Update SimpleBlinkG2.c

--- Simple Blink/MSP430G2553/SimpleBlinkG2.c
+++ Simple Blink/MSP430G2553/SimpleBlinkG2.c
@@ -3,9 +3,9 @@ int main()
 {
     WDTCTL = WDTPW + WDTHOLD; //Stop Watchdog timer
 
-    P1SEL &= ~BIT0;  //sets P1.0 to be configured as I/0
+    P1SEL &= ~BIT0;  //sets pin 0 and pin 1 as I/0
     P1SEL2 &= ~BIT0;
-    P1DIR |= BIT0;+
+    P1DIR |= BIT0; //Makes pin 0 high and pin 1 low
 
 
 
