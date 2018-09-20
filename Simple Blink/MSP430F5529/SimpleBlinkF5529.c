c8df2a13e54f778ce77b850f529719802c748f2c
okunj4 <36168902+okunj4@users.noreply.github.com>
2018-09-20 15:45:55 -0400

    Update SimpleBlinkF5529.c

--- Simple Blink/MSP430F5529/SimpleBlinkF5529.c
+++ Simple Blink/MSP430F5529/SimpleBlinkF5529.c
@@ -3,13 +3,13 @@ int main(void)
 {
     WDTCTL = WDTPW + WDTHOLD; //Stop Watchdog timer
 
-    P1SEL &= ~BIT0;
-    P1DIR |= BIT0;
+    P1SEL &= ~BIT0;  
+    P1DIR |= BIT0; 
 
     while(1)
     {
         P1OUT ^= BIT0;
-        _delay_cycles(100000);
+        _delay_cycles(100000); //sets delay so LED can be seen
     }
     return 0;
 }
