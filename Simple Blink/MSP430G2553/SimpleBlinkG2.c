946135031fdc28c1ad82994abd458415b4b607e5
okunj4 <36168902+okunj4@users.noreply.github.com>
2018-09-21 10:37:17 -0400

    Update SimpleBlinkG2.c

--- Simple Blink/MSP430G2553/SimpleBlinkG2.c
+++ Simple Blink/MSP430G2553/SimpleBlinkG2.c
@@ -1,10 +1,15 @@
+/*  Author: Jacob Okun
+    Created: September 12, 2018
+    Last Updated: September 21, 2018*/
+
+
 #include <msp430.h>
 int main()
 {
     WDTCTL = WDTPW + WDTHOLD; //Stop Watchdog timer
 
-    P1SEL &= ~BIT0;  //sets pin 0 and pin 1 as I/0
-    P1SEL2 &= ~BIT0;
+    P1SEL &= ~BIT0;  //sets Port 1 Select bit to zero
+    P1SEL2 &= ~BIT0; //sets Port 1 Select bit 2 to zero
     P1DIR |= BIT0; //Makes pin 0 high and pin 1 low
 
 
@@ -14,9 +19,9 @@ int main()
       {
         volatile unsigned int i;
 
-        P1OUT ^= BIT0;                          // Toggle P1.0 using exclusive-OR
+        P1OUT ^= BIT0;                          // Toggle P1.0 using exclusive-OR which turns the LED on and off
 
-        i = 50000;                              // Delay
+        i = 50000;                              // Delay that allows the LED to be seen
         do (i--);
         while (i != 0);
       }
