7afa3fbe3f27b1554c7891841a2395b2e1ee84c9
okunj4 <36168902+okunj4@users.noreply.github.com>
2018-09-21 10:40:01 -0400

    Update SimpleBlinkF5529.c
    Added comments

--- Simple Blink/MSP430F5529/SimpleBlinkF5529.c
+++ Simple Blink/MSP430F5529/SimpleBlinkF5529.c
@@ -1,14 +1,19 @@
+/*  Author: Jacob Okun
+    Created: September 19, 2018
+    Last Updated: September 21, 2018*/
+
+
 #include <msp430.h>
 int main(void)
 {
     WDTCTL = WDTPW + WDTHOLD; //Stop Watchdog timer
 
-    P1SEL &= ~BIT0;  
-    P1DIR |= BIT0; 
+    P1SEL &= ~BIT0;  //Changes port 1 select bit to zero allowing P1 output and direction to be selected
+    P1DIR |= BIT0;  //Changes Port 1 least significant it to 1 to make direction output
 
     while(1)
     {
-        P1OUT ^= BIT0;
+        P1OUT ^= BIT0; //X-OR to change least significant bit to turn LED on and off
         _delay_cycles(100000); //sets delay so LED can be seen
     }
     return 0;
